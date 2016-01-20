24e4c069169445711d5e7f433e39215dd89295fb
Raj Abishek <rajabishek@hotmail.com>
2016-01-20 23:10:51 +0530

    Adds the tag model and its eloquent relationships

---
+++ app/Tag.php
@@ -0,0 +1,25 @@
+<?php
+
+namespace Vuetricks;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Tag extends Model
+{
+    /**
+     * The table associated with the model.
+     *
+     * @var string
+     */
+    protected $table = 'tags';
+
+    /**
+     * Query the tricks that belong to the tag.
+     *
+     * @return \Illuminate\Database\Eloquent\Relations\BelongsToMany
+     */
+    public function tricks()
+    {
+        return $this->belongsToMany('Vuetricks\Trick');
+    }
+}
