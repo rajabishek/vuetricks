452842a33682195a804b1da234dcb896bcfcb75f
Raj Abishek <rajabishek@hotmail.com>
2016-01-22 19:45:59 +0530

    Adds the pivot table for the category and the tricks

---
+++ database/migrations/2016_01_17_134714_create_category_trick_table.php
@@ -0,0 +1,47 @@
+<?php
+
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCategoryTrickTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('category_trick', function (Blueprint $table) {
+            $table->increments('id');
+            $table->integer('category_id')->unsigned();
+            $table->integer('trick_id')->unsigned();
+            $table->timestamps();
+
+            $table->foreign('category_id')
+                  ->references('id')->on('categories')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+
+            $table->foreign('trick_id')
+                  ->references('id')->on('tricks')
+                  ->onUpdate('cascade')
+                  ->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('category_trick', function($table)
+        {
+            $table->dropForeign('category_trick_category_id_foreign');
+            $table->dropForeign('category_trick_trick_id_foreign');
+        });
+        Schema::drop('category_trick');
+    }
+}
