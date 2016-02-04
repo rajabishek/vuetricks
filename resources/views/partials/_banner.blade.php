5a5e37e54e08fbbbdb9ed4a7cdc6ca2485f9fb59
Raj Abishek <rajabishek@hotmail.com>
2016-02-05 01:00:37 +0530

    Partial to display the banner for the website

---
+++ resources/views/partials/_banner.blade.php
@@ -0,0 +1,5 @@
+<div class="jumbotron">
+    <h1>Vuetricks</h1>
+    <h5 class="lead">Vuetricks was created to help Vue community find and share interesting ways of using Vue framework</h5>
+    <a class="btn btn-sm btn-info" role="button" href="http://vuejs.org">Learn more »</a>
+</div>
