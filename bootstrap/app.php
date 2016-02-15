6ac00561c8084d50589d602fdfb2313772521441
Raj Abishek <rajabishek@hotmail.com>
2016-02-15 19:00:00 +0530

    Make minor changes while registering the bindings

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    App\Http\Kernel::class
+    Vuetricks\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    App\Console\Kernel::class
+    Vuetricks\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    App\Exceptions\Handler::class
+    Vuetricks\Exceptions\Handler::class
 );
 
 /*
