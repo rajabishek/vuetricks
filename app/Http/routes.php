55b6a32c5892c1687a05042788b3d202643ce91f
Raj Abishek <rajabishek@hotmail.com>
2016-02-16 19:21:30 +0530

    Adds the routes for auth and the admin routes to manage the users, tags and categories

--- app/Http/routes.php
+++ app/Http/routes.php
@@ -2,21 +2,6 @@
 
 /*
 |--------------------------------------------------------------------------
-| Routes File
-|--------------------------------------------------------------------------
-|
-| Here is where you will register all of the routes in an application.
-| It's a breeze. Simply tell Laravel the URIs it should respond to
-| and give it the controller to call when that URI is requested.
-|
-*/
-
-Route::get('/', function () {
-    return view('welcome');
-});
-
-/*
-|--------------------------------------------------------------------------
 | Application Routes
 |--------------------------------------------------------------------------
 |
@@ -27,5 +12,33 @@ Route::get('/', function () {
 */
 
 Route::group(['middleware' => ['web']], function () {
-    //
+
+    Route::group(['namespace' => 'Admin','prefix' => 'admin'], function () {
+    
+        Route::get('users', 'UsersController@index');
+        
+        Route::resource('tags', 'TagsController');
+        
+        Route::resource('categories', 'CategoriesController');
+    });
+
+	Route::get('/', ['as' => 'pages.home', 'uses' => 'PagesController@getHome']);
+   
+    Route::group(['namespace' => 'Auth', 'prefix' => 'auth'], function() {
+    	
+    	Route::get('register', ['as' => 'auth.getRegister', 'uses' => 'AuthController@getRegister']);
+   		Route::post('register', ['as' => 'auth.postRegister', 'uses' => 'AuthController@postRegister']);
+
+   		Route::get('login', ['as' => 'auth.getLogin', 'uses' => 'AuthController@getLogin']);
+   		Route::post('login', ['as' => 'auth.postlogin', 'uses' => 'AuthController@postLogin']);
+
+        Route::get('github', ['as' => 'auth.github', 'uses' => 'AuthController@redirectToProvider']);
+        Route::get('github/callback', 'AuthController@handleProviderCallback');
+    
+    	Route::get('logout', [ 'as' => 'auth.logout', 'uses' => 'AuthController@getLogout']);
+    });
+
+    Route::get('tricks/{trick_slug?}', [ 'as' => 'tricks.show', 'uses' => 'TricksController@getShow' ]);
+    Route::post('tricks/{trick_slug}/like', [ 'as' => 'tricks.like', 'uses' => 'TricksController@postLike']);
+    Route::resource('user/tricks','UserTricksController', ['except' => 'show']);
 });
